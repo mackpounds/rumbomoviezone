30bb66a743b33d1e14cb99abe2425eaeb5edc17d
Patrick Alexander <igiliegbepatrick@gmail.com>
2024-06-11 13:06:01 +0100

    Delete main.py

--- main.py
+++
@@ -1,9 +0,0 @@
-import os
-from WEBSITE_PACKAGE import create_app
-
-app = create_app()
-
-if __name__ == "__main__":
-    host = os.getenv("HOST", "0.0.0.0")
-    port = int(os.getenv("PORT", 5000))
-    app.run(host=host, port=port, debug=os.getenv("DEBUG", "FALSE" == "TRUE"))
